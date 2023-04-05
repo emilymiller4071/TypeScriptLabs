f739061b37eaba0eb12982b5cb1f058eb36aad59
Emily Miller <emilyrauch3@gmail.com>
2023-04-05 13:06:17 -0400

    arrays

---
+++ arrays.js
@@ -0,0 +1,10 @@
+"use strict";
+var numberSet = [1, 2, 3];
+// let [b, c, d] = numberSet;
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// if you don't need an item just skip that item in assignment
+var c = numberSet[1], d = numberSet[2];
+console.log(c);
+console.log(d);
---
+++ arrays.ts
@@ -0,0 +1,13 @@
+let numberSet = [1, 2, 3];
+
+// let [b, c, d] = numberSet;
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+
+
+// if you don't need an item just skip that item in assignment
+let [, c, d] = numberSet;
+console.log(c);
+console.log(d);
