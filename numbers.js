ddbdb7710de7ec9562901cde56256619fab599de
Emily Miller <emilyrauch3@gmail.com>
2023-04-05 09:36:18 -0400

    lab for scope, changed noEmitOnError to false to allow the file to stay

---
+++ numbers.js
@@ -0,0 +1,8 @@
+"use strict";
+var numbers2 = [1, 2, 3, 4];
+for (var counter2 = 0; counter2 < numbers2.length; counter2++) {
+    console.log(numbers2[counter2]);
+}
+console.log("at end: " + counter2);
+// code cannot find counter2
+//this lab deals with scope
