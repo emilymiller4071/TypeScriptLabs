5b7b9b376c6b9df190212d2d4073747cd7262cfc
emilymiller4071 <emilyrauch3@gmail.com>
2023-04-05 20:39:12 -0400

    ArrowFunctions2 and decorators

--- js/ArrowFunctions2.js
+++ js/ArrowFunctions2.js
@@ -5,3 +5,11 @@ function sum(a, b) {
 let result = sum(1, 2);
 console.log(result);
 const sum1 = (a, b) => { return a + b; };
+let evens = [0, 2, 4, 6, 8];
+let evensDoubled = [];
+for (let index = 0; index < evens.length; index++) {
+    evensDoubled[index] = evens[index] * 2;
+}
+evens.forEach(num => {
+    evensDoubled.push(num * 2);
+});
